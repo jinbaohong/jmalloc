e74dcdf387ce61e4175d4d860aa2c8caab8ae19e
jinbaohong <satanetetet666@gmail.com>
2021-02-11 16:40:54 +0800

    jfree bug fix

--- main.c
+++ main.c
@@ -11,7 +11,7 @@
 #define BLK_GET_NEXT_BLK(ptr) ((ptr) + (*BLK_GET_HEAD_PTR(ptr) & ~(0x7)))
 #define BLK_GET_PREV_BLK(ptr) ((ptr) - (*BLK_GET_HEAD_PTR((ptr) - sizeof(size_j)) & ~(0x7)))
 #define BLK_IS_ALLOCATED(ptr) (*BLK_GET_HEAD_PTR(ptr) & O_ALLOCATED)
-#define BLK_IS_ENOUGH(ptr, blk_siz) (BLK_GET_SIZE(ptr) > blk_siz)
+#define BLK_IS_ENOUGH(ptr, blk_siz) (BLK_GET_SIZE(ptr) >= blk_siz)
 #define BLK_SET_HEAD(ptr, blk_siz, flag) *BLK_GET_HEAD_PTR(ptr) = (blk_siz) | flag
 #define BLK_SET_FOOT(ptr, blk_siz, flag) *BLK_GET_FOOT_PTR(ptr) = (blk_siz) | flag
 
@@ -24,12 +24,13 @@ static void* last_block;
 void *first_fit(size_j blk_siz)
 {
 	void *blk_ptr;
-
+	int remain_space;
 	for (blk_ptr = heap_start; blk_ptr != last_block; blk_ptr = BLK_GET_NEXT_BLK(blk_ptr))
 	{
 		if (!BLK_IS_ALLOCATED(blk_ptr) && BLK_IS_ENOUGH(blk_ptr, blk_siz)) {
-			size_j remain_space = BLK_GET_SIZE(blk_ptr) - blk_siz - 2*sizeof(size_j);
-			assert(remain_space >= 0);
+			printf("blk_ptr=%p is not allocated and enough\n", blk_ptr);
+			remain_space = BLK_GET_SIZE(blk_ptr) - blk_siz - 2*sizeof(size_j);
+			assert(remain_space >= -8);
 			/* If the left space is enough to form another block, then split it. */
 			if (remain_space > 0) {
 				printf("split\n");
@@ -42,8 +43,8 @@ void *first_fit(size_j blk_siz)
 			}
 			else {
 				printf("can't split\n");
-				BLK_SET_HEAD(blk_ptr, blk_siz + 4*sizeof(size_j), O_ALLOCATED);
-				BLK_SET_FOOT(blk_ptr, blk_siz + 4*sizeof(size_j), O_ALLOCATED);
+				BLK_SET_HEAD(blk_ptr, BLK_GET_SIZE(blk_ptr) + 2*sizeof(size_j), O_ALLOCATED);
+				BLK_SET_FOOT(blk_ptr, BLK_GET_SIZE(blk_ptr) + 2*sizeof(size_j), O_ALLOCATED);
 			}
 			return blk_ptr;
 		}
@@ -63,7 +64,7 @@ int heap_extend()
 	BLK_SET_HEAD(last_block, 2*sizeof(size_j), O_ALLOCATED);
 	BLK_SET_FOOT(last_block, 2*sizeof(size_j), O_ALLOCATED);
 	heap_end = last_block + 2*sizeof(size_j);
-	printf("heap_end=%p, sbrk(0)=%p\n", heap_end, sbrk(0));
+
 	return 0;
 }
 
@@ -90,7 +91,8 @@ void *jmalloc(size_j size)
 
 	resize = size % 8 ? size + 8 - (size % 8) : size;
 	// blk_siz = resize + 2*sizeof(size_j);
-	assert(!(blk_siz % 8));
+	printf("blk_siz=%d\n", resize);
+	assert(!(resize % 8));
 
 	while (!(res_ptr = first_fit(resize))) {
 		if (heap_extend())
@@ -109,6 +111,23 @@ void jfree(void *ptr)
 	BLK_SET_FOOT(blk_ptr, (*BLK_GET_HEAD_PTR(blk_ptr) & ~(0x7)), 0);
 }
 
+void jcheck(void)
+{
+	void *blk_ptr;
+	// int cnt = 5;
+	printf("Start checking...\n");
+	for (blk_ptr = heap_start; blk_ptr != last_block; blk_ptr = BLK_GET_NEXT_BLK(blk_ptr))
+	{	
+		// if (cnt-- == 0)
+		// 	break;
+		printf("blk_ptr = %p\n", blk_ptr);
+		printf("header  = %d\n", *BLK_GET_HEAD_PTR(blk_ptr));
+		printf("footer  = %d\n", *BLK_GET_FOOT_PTR(blk_ptr));
+		printf("------------\n");
+	}
+	printf("Checked finished!\n");
+}
+
 int main(int ac, char const *av[])
 {
 	printf("**********************************************************************\n");
@@ -119,19 +138,21 @@ int main(int ac, char const *av[])
 	printf("**********************************************************************\n");
 	printf("**********************************************************************\n");
 	heap_init();
-
 	int *i, *j;
 	i = jmalloc(8*sizeof(int));
 	printf("heap_start at %p\n", heap_start);
 	printf("heap_end   at %p\n", heap_end);
 	printf("last_block at %p\n", last_block);
 	printf("Allocate i at %p\n", i);
-
+	jcheck();
+	jfree(i);
+	jcheck();
 	j = jmalloc(8*sizeof(long));
 	printf("heap_start at %p\n", heap_start);
 	printf("heap_end   at %p\n", heap_end);
 	printf("last_block at %p\n", last_block);
 	printf("Allocate i at %p\n", j);
+	jcheck();
 
 	return 0;
 }
