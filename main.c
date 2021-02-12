7058b8a1f7655974b3ddbb46edf2928481905806
jinbaohong <satanetetet666@gmail.com>
2021-02-12 17:41:49 +0800

    jmalloc works: big request bug fix

--- main.c
+++ main.c
@@ -19,9 +19,68 @@ typedef unsigned int size_j;
 #define BLK_SET_FOOT(ptr, blk_siz, flag) *BLK_GET_FOOT_PTR(ptr) = (blk_siz) | flag
 
 
+void jfree(void *ptr);
+void *first_fit(size_j blk_siz);
+int heap_extend();
+int heap_init();
+void *jmalloc(size_j size);
+void jcheck(void);
+
+
+
 static void* heap_start;
+static void* first_block;
 static void* heap_end;
 static void* last_block;
+
+void jfree(void *ptr)
+{
+    void *blk_ptr;
+    size_j val, next_alloc, prev_alloc;
+    
+    blk_ptr = ptr - sizeof(size_j);
+    next_alloc = BLK_IS_ALLOCATED(BLK_GET_NEXT_BLK(blk_ptr));
+    prev_alloc = BLK_IS_ALLOCATED(BLK_GET_PREV_BLK(blk_ptr));
+    printf("blk_ptr=%p, next_alloc=%d, prev_alloc=%d\n", blk_ptr, next_alloc, prev_alloc);
+    val = *BLK_GET_HEAD_PTR(blk_ptr) & ~0x7;
+
+    if (prev_alloc && !next_alloc) // only next block is free.
+    {
+        printf("jfree: only next block is free\n");
+        val += *BLK_GET_HEAD_PTR(BLK_GET_NEXT_BLK(blk_ptr));
+        BLK_SET_HEAD(blk_ptr, val, 0);
+        BLK_SET_FOOT(blk_ptr, val, 0);
+    }
+
+    else if (!prev_alloc && next_alloc) // only prev block is free.
+    {
+        printf("jfree: only prev block is free\n");
+        val += *BLK_GET_HEAD_PTR(BLK_GET_PREV_BLK(blk_ptr));
+        // BLK_SET_FOOT(blk_ptr, val, 0);
+        // BLK_SET_HEAD(BLK_GET_HEAD_PTR(BLK_GET_PREV_BLK(blk_ptr)), val, 0);
+        blk_ptr = BLK_GET_PREV_BLK(blk_ptr);
+        BLK_SET_HEAD(blk_ptr, val, 0);
+        BLK_SET_FOOT(blk_ptr, val, 0);
+
+    }
+
+    else if (prev_alloc && next_alloc) // need no merge.
+    {
+        printf("jfree: no need merge\n");
+        BLK_SET_HEAD(blk_ptr, val, 0);
+        BLK_SET_FOOT(blk_ptr, val, 0);
+    }
+
+    else
+    {
+        printf("jfree: both need merge\n");
+        val += (*BLK_GET_HEAD_PTR(BLK_GET_PREV_BLK(blk_ptr)) +
+                *BLK_GET_HEAD_PTR(BLK_GET_NEXT_BLK(blk_ptr)));
+        blk_ptr = BLK_GET_HEAD_PTR(BLK_GET_PREV_BLK(blk_ptr));
+        BLK_SET_HEAD(blk_ptr, val, 0);
+        BLK_SET_FOOT(blk_ptr, val, 0);
+    }
+}
 
 void *first_fit(size_j blk_siz)
 {
@@ -56,29 +115,27 @@ void *first_fit(size_j blk_siz)
 
 int heap_extend()
 {
-    void *ptr;
+    void *ptr, *blk_ptr;
     if ((ptr = sbrk(CHUNK_SIZE)) == (void*)(-1))
         return -1;
 
-    BLK_SET_HEAD(last_block, CHUNK_SIZE, 0);
-    BLK_SET_FOOT(last_block, CHUNK_SIZE, 0);
-    last_block = BLK_GET_NEXT_BLK(last_block);
+    blk_ptr = last_block;
+    BLK_SET_HEAD(blk_ptr, CHUNK_SIZE, 0);
+    BLK_SET_FOOT(blk_ptr, CHUNK_SIZE, 0);
+    last_block = BLK_GET_NEXT_BLK(blk_ptr);
     BLK_SET_HEAD(last_block, 2*sizeof(size_j), O_ALLOCATED);
     BLK_SET_FOOT(last_block, 2*sizeof(size_j), O_ALLOCATED);
+    // last_block = BLK_GET_NEXT_BLK(last_block);
+    // BLK_SET_HEAD(last_block, 2*sizeof(size_j), O_ALLOCATED);
+    // BLK_SET_FOOT(last_block, 2*sizeof(size_j), O_ALLOCATED);
+
+
     heap_end = last_block + 2*sizeof(size_j);
 
-    return 0;
-}
 
-int heap_init()
-{
-    heap_start = sbrk(3*sizeof(size_j));
-    heap_start += sizeof(size_j);
-    last_block = heap_start;
-    BLK_SET_HEAD(last_block, 2*sizeof(size_j), O_ALLOCATED);
-    BLK_SET_FOOT(last_block, 2*sizeof(size_j), O_ALLOCATED);
-    heap_end = heap_start + 2*sizeof(size_j);
 
+    jfree(blk_ptr+sizeof(size_j));
+    // jcheck();
     return 0;
 }
 
@@ -97,67 +154,40 @@ void *jmalloc(size_j size)
     assert(!(resize % 8));
 
     while (!(res_ptr = first_fit(resize))) {
-        if (heap_extend())
+        printf("first_fit fail\n");
+        if (heap_extend()){
+            printf("heap extend fail\n");
             return NULL;
+        }
     }
 
     return res_ptr + sizeof(size_j);
 }
 
-void jfree(void *ptr)
+
+int heap_init()
 {
-    void *blk_ptr;
-    size_j val, next_alloc, prev_alloc;
-    
-    blk_ptr = ptr - sizeof(size_j);
-    next_alloc = BLK_IS_ALLOCATED(BLK_GET_NEXT_BLK(blk_ptr));
-    prev_alloc = BLK_IS_ALLOCATED(BLK_GET_PREV_BLK(blk_ptr));
-    val = *BLK_GET_HEAD_PTR(blk_ptr) & ~0x7;
-
-    if (prev_alloc && !next_alloc) // only next block is free.
-    {
-        printf("jfree: only next block is free\n");
-        val += *BLK_GET_HEAD_PTR(BLK_GET_NEXT_BLK(blk_ptr));
-        BLK_SET_HEAD(blk_ptr, val, 0);
-        BLK_SET_FOOT(blk_ptr, val, 0);
-    }
-
-    else if (!prev_alloc && next_alloc) // only prev block is free.
-    {
-        printf("jfree: only prev block is free\n");
-        val += *BLK_GET_HEAD_PTR(BLK_GET_PREV_BLK(blk_ptr));
-        // BLK_SET_FOOT(blk_ptr, val, 0);
-        // BLK_SET_HEAD(BLK_GET_HEAD_PTR(BLK_GET_PREV_BLK(blk_ptr)), val, 0);
-        blk_ptr = BLK_GET_HEAD_PTR(BLK_GET_PREV_BLK(blk_ptr));
-        BLK_SET_HEAD(blk_ptr, val, 0);
-        BLK_SET_FOOT(blk_ptr, val, 0);
-
-    }
-
-    else if (prev_alloc && next_alloc) // need no merge.
-    {
-        printf("jfree: no need merge\n");
-        BLK_SET_HEAD(blk_ptr, val, 0);
-        BLK_SET_FOOT(blk_ptr, val, 0);
-    }
-
-    else
-    {
-        printf("jfree: both need merge\n");
-        val += (*BLK_GET_HEAD_PTR(BLK_GET_PREV_BLK(blk_ptr)) +
-                *BLK_GET_HEAD_PTR(BLK_GET_NEXT_BLK(blk_ptr)));
-        blk_ptr = BLK_GET_HEAD_PTR(BLK_GET_PREV_BLK(blk_ptr));
-        BLK_SET_HEAD(blk_ptr, val, 0);
-        BLK_SET_FOOT(blk_ptr, val, 0);
-    }
+    heap_start = sbrk(5*sizeof(size_j));
+    heap_start += sizeof(size_j);
+    first_block = heap_start;
+    BLK_SET_HEAD(heap_start, 2*sizeof(size_j), O_ALLOCATED);
+    BLK_SET_FOOT(heap_start, 2*sizeof(size_j), O_ALLOCATED);
+    heap_start += 2*sizeof(size_j);
+    last_block = heap_start;
+    BLK_SET_HEAD(last_block, 2*sizeof(size_j), O_ALLOCATED);
+    BLK_SET_FOOT(last_block, 2*sizeof(size_j), O_ALLOCATED);
+    heap_end = heap_start + 2*sizeof(size_j);
+    jcheck();
+    return 0;
 }
+
 
 void jcheck(void)
 {
     void *blk_ptr;
     // int cnt = 5;
     printf("Start checking...\n");
-    for (blk_ptr = heap_start; blk_ptr != last_block; blk_ptr = BLK_GET_NEXT_BLK(blk_ptr))
+    for (blk_ptr = first_block; blk_ptr != heap_end; blk_ptr = BLK_GET_NEXT_BLK(blk_ptr))
     {    
         // if (cnt-- == 0)
         //     break;
@@ -184,16 +214,22 @@ int main(int ac, char const *av[])
     printf("heap_start at %p\n", heap_start);
     printf("heap_end   at %p\n", heap_end);
     printf("last_block at %p\n", last_block);
+        printf("last_block header  = %d\n", *BLK_GET_HEAD_PTR(last_block));
+        printf("last_block footer  = %d\n", *BLK_GET_FOOT_PTR(last_block));
     printf("Allocate i at %p\n", i);
-    j = jmalloc(8*sizeof(int));
+    j = jmalloc(8135);
     printf("heap_start at %p\n", heap_start);
     printf("heap_end   at %p\n", heap_end);
     printf("last_block at %p\n", last_block);
+        printf("last_block header  = %d\n", *BLK_GET_HEAD_PTR(last_block));
+        printf("last_block footer  = %d\n", *BLK_GET_FOOT_PTR(last_block));
     printf("Allocate j at %p\n", j);
     k = jmalloc(8*sizeof(int));
     printf("heap_start at %p\n", heap_start);
     printf("heap_end   at %p\n", heap_end);
     printf("last_block at %p\n", last_block);
+        printf("last_block header  = %d\n", *BLK_GET_HEAD_PTR(last_block));
+        printf("last_block footer  = %d\n", *BLK_GET_FOOT_PTR(last_block));
     printf("Allocate k at %p\n", k);
     jcheck();
     jfree(i);
